84b18b2b2cce77e91129fe9fe334409269e4e9ab
Daniel <dhcgn@users.noreply.github.com>
2023-11-07 18:19:25 +0100

    Add Update-PwshRepo function to collection_Suite.ps1

--- scripts/tools/collection_Suite.ps1
+++ scripts/tools/collection_Suite.ps1
@@ -3,3 +3,11 @@ function global:New-Guid {
 }
 
 Set-Alias -Name guid -Value New-Guid -Scope Global
+
+function Update-PwshRepo {
+    $f = "C:\dev\pwsh-repo\"
+    if (Test-Path $f) {
+        Write-Host "Pulling from $f"
+        git -C $f pull
+    }
+}
