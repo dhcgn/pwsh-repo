98bdccd233110f8d75476150d9393f7974bbcf50
Daniel <dhcgn@users.noreply.github.com>
2024-02-03 20:13:58 +0100

    Add add synopsis for Set-PowerPointExportResolution.ps1 script to modify export resolution

--- scripts/tools/Set-PowerPointExportResolution.ps1
+++ scripts/tools/Set-PowerPointExportResolution.ps1
@@ -1,3 +1,16 @@
+<#
+.SYNOPSIS
+    This script changes the export resolution of PowerPoint slides.
+
+.DESCRIPTION
+    The Set-PowerPointExportResolution.ps1 script changes the export resolution of PowerPoint slides by modifying a registry key. The new resolution is set to 300 DPI. If the registry key does not exist, it is created.
+
+.EXAMPLE
+    .\Set-PowerPointExportResolution.ps1
+
+    This will set the export resolution of PowerPoint slides to 300 DPI.
+
+#>
 # https://docs.microsoft.com/en-us/office/troubleshoot/powerpoint/change-export-slide-resolution
 $path = "HKCU:\Software\Microsoft\Office\16.0\PowerPoint\Options"
 $old = Get-ItemProperty -Path $path  -ErrorAction Ignore | ForEach-Object { $_.ExportBitmapResolution }
