34f35f6e90616570cb431a3eac88b02247ca77f4
Daniel <dhcgn@users.noreply.github.com>
2024-02-03 20:10:12 +0100

    Add Get-RandomSecure.ps1 script for generating secure random strings

--- scripts/crypto/Get-RandomSecure.ps1
+++ scripts/crypto/Get-RandomSecure.ps1
@@ -1,3 +1,19 @@
+<#
+.SYNOPSIS
+    This script generates a secure random string.
+
+.DESCRIPTION
+    The Get-RandomSecure.ps1 script generates a secure random string using the RNGCryptoServiceProvider class from the System.Security.Cryptography namespace. The strength of the generated string can be specified as a parameter. If no strength is provided or if the provided strength is less than 1, the default strength of 32 characters (256 bits) is used. The generated string is base64 encoded and safe for URL use, as '+' and '/' characters are replaced with '-' and '_', respectively, and trailing '=' characters are removed.
+
+.PARAMETER Strength
+    The strength of the random string to be generated. This is the length of the string in bytes. If not provided or if less than 1, a default strength of 32 characters (256 bits) is used.
+
+.EXAMPLE
+    .\Get-RandomSecure.ps1 -Strength 16
+
+    This will generate a secure random string of 16 bytes in length.
+
+#>
 param (
     [int]
     $Strength
